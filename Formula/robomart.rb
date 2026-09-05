5c32e4dc558a131eb0776140dcf28273955243d4
Ali A. <174729389+roboalias@users.noreply.github.com>
2026-09-06 03:27:47 +0500

    Update Robomart Command to 1.2.2

--- Formula/robomart.rb
+++ Formula/robomart.rb
@@ -4,9 +4,9 @@
 class Robomart < Formula
   desc "Agentic commerce and delivery from the terminal"
   homepage "https://robomart.ai"
-  url "https://registry.npmjs.org/@robomart/command/-/command-1.2.1.tgz"
-  version "1.2.1"
-  sha256 "87b69563b23c76280e1cc45ccb55086cff151905ab7d7df974784cd640c023f7"
+  url "https://registry.npmjs.org/@robomart/command/-/command-1.2.2.tgz"
+  version "1.2.2"
+  sha256 "25fa73e09181d46e2544c49b061dbae1cd7d2146b0443147516db7dcd0460f15"
   license "Apache-2.0"
 
   depends_on :macos
@@ -17,6 +17,6 @@ class Robomart < Formula
   end
 
   test do
-    assert_match "1.2.1", shell_output("#{bin}/robomart --version")
+    assert_match "1.2.2", shell_output("#{bin}/robomart --version")
   end
 end
